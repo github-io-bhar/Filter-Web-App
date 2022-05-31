85e7f4bb95fed64070f8b4aabe20b478e2f87e15
github-io-bhar <86400328+github-io-bhar@users.noreply.github.com>
2022-05-31 16:47:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
+mustacheX = 0;
+mustacheY = 0;
 function preload() {
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
-
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
@@ -10,23 +12,22 @@ function setup() {
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
-
 function modelLoaded() {
     console.log('PoseNet Is Initialized');
 }
-
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        mustacheX = results[0].pose.nose.x;
+        mustacheY = results[0].pose.nose.y;
+        console.log("nose x = " + mustacheX);
+        console.log("nose y = " + mustacheY);
     }
 }
-
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(mustache, mustacheX, mustacheY, 30, 30);
 }
-
 function take_snapshot() {
     save('myFilterImage.png');
 }
